3a0e178d1ed1f2f67d6f6912f1ae087a4e7fa4ae
qlevaslot <quentin.levaslot@gmail.com>
2016-11-10 23:43:08 +0100

    build: add babel compilation to polymer build task

--- gulp-tasks/clean.js
+++ gulp-tasks/clean.js
@@ -1,9 +1,7 @@
-var gulp = require('gulp');
 var del = require('del');
 
-gulp.task('clean', (cb) => {
-  del([global.config.build.rootDirectory], {dot: true})
-    .then(() => {
-      cb();
-    });
-});
+function clean() {
+  return del([global.config.build.rootDirectory], {dot: true});
+}
+
+module.exports = clean;
